6a8ac3f7bbc5d745f811fd2f7dd29185285d4653
enguerrand - deligraph <graphicdesign@deligraph.com>
2022-05-08 22:51:58 +0200

    fix lerp ratio

--- js/custom/scroll.js
+++ js/custom/scroll.js
@@ -2,7 +2,7 @@ const init = () => {
   var scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
-    lerp: 0.02,
+    lerp: 0.009,
     smartphone: {
       smooth: true,
     },
