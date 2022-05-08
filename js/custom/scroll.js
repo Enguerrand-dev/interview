18f9381b782121a0e4bf3c7066b47e2eedfa50e8
enguerrand - deligraph <graphicdesign@deligraph.com>
2022-05-08 18:17:49 +0200

    fix animation lerp

--- js/custom/scroll.js
+++ js/custom/scroll.js
@@ -1,9 +1,11 @@
-(function () {
+const init = () => {
   var scroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
+    lerp: 0.02,
     smartphone: {
       smooth: true,
     },
   });
-})();
+};
+init();
