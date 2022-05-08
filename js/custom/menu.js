5ba3f02a53913c92b2afaad24788a50cb171bf1f
enguerrand - deligraph <graphicdesign@deligraph.com>
2022-05-08 15:12:55 +0200

    final-build

--- js/custom/menu.js
+++ js/custom/menu.js
@@ -1,18 +1,44 @@
 const btn = document.getElementById("btn-menu");
 const menu = document.getElementById("menu");
 
+const menu_item = document.getElementsByClassName("menu__small__item");
+
 // TODO: add scroll block when menu is open
 
-btn.onclick = function () {
+btn.onclick = () => {
   menu.classList.toggle("open");
-
   if (menu.classList.contains("open")) {
     // change text btn
-    btn.innerHTML = "Menu";
-
-    gsap.to(menu, { duration: 0.1, opacity: 0 });
-  } else {
     btn.innerHTML = "Close";
-    gsap.to(menu, { duration: 0.1, opacity: 1 });
+    // create new timeline
+    const tl = gsap.timeline();
+    tl.to(menu, { duration: 0.1, opacity: 1 });
+    open(tl);
+  } else {
+    btn.innerHTML = "Menu";
+    gsap.to(menu, { duration: 0.1, opacity: 0 });
+    close();
   }
 };
+
+const open = (tl) => {
+  tl.fromTo(
+    ".small__language",
+    { duration: 0.5, opacity: 0, x: 10 },
+    { duration: 0.5, opacity: 1, x: 0 }
+  );
+  for (const item of menu_item) {
+    tl.fromTo(
+      item,
+      { duration: 0.1, opacity: 0, x: 100 },
+      { duration: 0.33, opacity: 1, x: 0 }
+    );
+  }
+  tl.fromTo(
+    "#menu__cta",
+    { duration: 0.1, opacity: 0, y: 10 },
+    { duration: 0.1, opacity: 1, y: 0 }
+  );
+};
+
+const close = (tl) => {};
---
+++ js/custom/scroll.js
@@ -0,0 +1,9 @@
+(function () {
+  var scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    smartphone: {
+      smooth: true,
+    },
+  });
+})();
